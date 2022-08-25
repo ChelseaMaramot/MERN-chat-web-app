6998b9e71c17fd5a2f87f305c5005d28c7a60c26
ChelseaMaramot <maramotc@mcmaster.ca>
2022-08-24 21:41:55 -0400

    Added User schema

---
+++ backend/model/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        min: 6,
+        max: 30,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        max: 15,
+        required: true,
+    },
+    password: {
+        type: String,
+        unique: false,
+        required: true,
+        min: 6,
+    }
+})
