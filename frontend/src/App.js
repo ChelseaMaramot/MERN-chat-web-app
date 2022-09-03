40c4dda8c79299c40280e3b8c2997131ae422c0e
ChelseaMaramot <maramotc@mcmaster.ca>
2022-09-02 22:55:51 -0400

    added nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Message from './components/UI/Message/MessageCard';
 import SendMessage from './components/SendMessage/SendMessage';
+import NavBar from './components/UI/NavBar/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar></NavBar>
        <Message
         newMessage = 'how are you?'
        ></Message>
---
+++ frontend/src/components/UI/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './NavBar.css'
+
+const NavBar = (props) => {
+    return(
+        <div className="header">
+            <h3>Alora Bustos</h3>
+        </div>
+    )
+}
+
+export default NavBar;
