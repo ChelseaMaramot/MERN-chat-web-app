18a450c1df0074df7e2814d196dbfdc056744bb0
ChelseaMaramot <maramotc@mcmaster.ca>
2022-09-22 21:51:58 -0400

    new information on side bar

--- frontend/src/components/UI/SideBar/ChatRoomBox.js
+++ frontend/src/components/UI/SideBar/ChatRoomBox.js
@@ -7,7 +7,14 @@ function ChatRoomBox() {
             <img className="chatRoomImg" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" alt=" "></img>
            
             <div className='textWrapper'>
-                <span className="chatName">Jane Doe</span>
+                <div className="boxTop">
+                    <span className="chatName">Jane Doe</span>
+                    <span className="timespan">30 mins ago</span>
+                </div>
+                <div className='boxBottom'>
+                    <span className='textPrev'>Lorem ipsum dolor sit amet</span>
+                </div>
+
             </div>
             
         </div>
