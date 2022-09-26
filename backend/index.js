4cef08104ee4988ba43316a184057ee1f3d2263f
ChelseaMaramot <maramotc@mcmaster.ca>
2022-09-25 22:20:11 -0400

    imported cors

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 const express = require("express");
 const messagesRoute = require('./routes/messagesRoute');
 const roomRoute = require('./routes/roomRoute');
+const cors = require('cors');
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 
 PORT = process.env.PORT || 3000
