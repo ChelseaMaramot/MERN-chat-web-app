495dad79ef4547d796bd3410454a4f5d29544a28
chelsea maramot <maramotc@mcmaster.ca>
2022-10-10 22:39:24 -0400

    created reg user api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,22 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../model/User');
 const UserSchema = require('../model/User');
 
 //Method: POST
 exports.registerUser= asyncHandler(async(req, res) => {
     try{
+        const { username, email, password } = req.body;
+
+        const emailExists = await User.findOne({ email });
+
+        if (emailExists){
+            res.status(500);
+            throw new Error ("Email Exists")
+        }
+
+        const newUser = await UserSchema.create(req.body);
         
+        res.status(200).json(newUser);
     } catch(err){
         res.status(500).json(err);
     } 
