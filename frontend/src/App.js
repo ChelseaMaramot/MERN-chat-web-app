019003835a21a87d6d4e7860aaec16002e9bb09c
chelsea maramot <maramotc@mcmaster.ca>
2022-10-15 21:07:52 -0400

    updated import statements for Auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './components/Authentication/Login';
 import Chat from './components/MainChat/Chat';
 import NavBar from './components/UI/NavBar/NavBar';
-import SignUp from './components/Authentication/Register';
+import SignUp from './components/Authentication/Auth';
 
 function App() {
   return (
