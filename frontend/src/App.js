41b81aa998fe9e9b08399a43931e66e987678342
chelsea maramot <maramotc@mcmaster.ca>
2022-10-16 20:42:38 -0400

    React router now working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './components/Authentication/Login';
 import Chat from './components/MainChat/Chat';
-import NavBar from './components/UI/NavBar/NavBar';
 import SignUp from './components/Authentication/Auth';
+import NavBar from './components/UI/NavBar/NavBar';
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/register" element={<SignUp></SignUp>}/>
-          <Route path="/chat" element={<Chat></Chat>}/>
+
+          <Route path="/chat/:id" element={<Chat></Chat>}/>
         </Routes>
     </Router>
   );
