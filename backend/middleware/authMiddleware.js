691cea55bcd8a48cf65ef5a14f4a376562cf8386
chelsea maramot <maramotc@mcmaster.ca>
2022-10-19 23:49:19 -0400

    generate token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,10 @@
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
+function generate_token(data){
+    return jwt.sign(data,process.env.TOKEN_SECRET, {expiresIn: '1800s'})
+}
+
 module.exports.authenticateToken = function(req, res, next){
     crypto.randomBytes(64, (err, buf) => {
         if (err){
