9a8a59711ea55046161daf1618b8d4f8845e5a19
chelsea maramot <maramotc@mcmaster.ca>
2022-10-20 23:53:39 -0400

    in the process of creating auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,11 +1,29 @@
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const dotenv = require('dotenv');
+dotenv.config();
 
+// token will be valid for 30 mins
 function generate_token(data){
     return jwt.sign(data,process.env.TOKEN_SECRET, {expiresIn: '1800s'})
 }
 
 module.exports.authenticateToken = function(req, res, next){
+    // client sends Authorization: Basic token
+    const authHeader = req.Header['authorization'];
+    const token = authHeader && authHeader.split('')[1];
+
+    if (!token) return res.status(401);
+    console.log(token);
+
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
+        console.log(err);
+        if (err) return res.status(403);
+
+        req.username = user
+    })
+
+    /*
     crypto.randomBytes(64, (err, buf) => {
         if (err){
             console.log(err)
@@ -13,5 +31,6 @@ module.exports.authenticateToken = function(req, res, next){
         }
         console.log(`The auth string is:`, buf.toString('hex'));
     }) 
+    */
 };
 
