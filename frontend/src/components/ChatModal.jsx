8bb7513f5af6bb12085fcce1b5d3abacd08d5e74
Chelsea <maramotc@mcmaster.ca>
2022-11-25 22:14:44 -0500

    added basic input and title to modal

--- frontend/src/components/ChatModal.jsx
+++ frontend/src/components/ChatModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Modal, Box,Typography } from "@mui/material";
+import { Button, TextField, Modal, Box,Typography } from "@mui/material";
 
 const style = {
     position: 'absolute',
@@ -8,7 +8,7 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    borderRadius: '20px',
     boxShadow: 24,
     p: 4,
   };
@@ -17,37 +17,38 @@ const style = {
 
 export default function ChatModal(props){
 
-    
     const [groupChatName, setGroupName] = useState();
     const [selectedUsers, setSelectedUsers] = useState([]);
     const [search, setSearch] = useState("");
     const [searchResult, setSearchResult] = useState([])
-    
-
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => {
-        setOpen(false)
-   
-    };
-  
-    
+ 
     return (
         <div>
             
             <Modal
-            open={props.isOpen}
+            open={true}
             onClose={props.closeModal}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
             >
             <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
+                <Typography id="modal-modal-title" variant="h6" component="h2" m={2} align='center'>
+                Create Group Chat
                 </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': { m: .75},
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    >
+                    <TextField id="group_name" label="Chat Name" color="secondary" variant="outlined" fullWidth />
+                    <TextField id="users" label="Add Users eg: John, Jane" color="secondary" variant="outlined" fullWidth/>
+                    <Button variant="contained" color="secondary" align="right" sx={{float: "right"}}>Create Chat</Button>
+            
+                </Box>
+                
             </Box>
             </Modal>
         </div>
