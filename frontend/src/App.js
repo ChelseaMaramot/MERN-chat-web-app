38f164fc8905dcf584e0d4fdf16f257acf7412e1
ChelseaMaramot <gracechelsea76@yahoo.com>
2022-11-27 18:30:38 -0500

    deleted unused files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
-import Login from './components/Authentication/Login';
 import Chat from './components/ChatDashboard';
-import Auth from './components/Authentication/Auth';
-import NavBar from './components/UI/NavBar/NavBar';
-import Sidebar from './components/Sidebar';
-import SideNavbar from './components/SideNavbar';
 
 
 function App() {
